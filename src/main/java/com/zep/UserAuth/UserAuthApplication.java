49640fbeedd4df628713a7469560dacd383b3c55
Felix Ojiambo <felixojiamboe@gmail.com>
2024-03-13 23:00:25 -0700

    initial setup

---
+++ src/main/java/com/zep/UserAuth/UserAuthApplication.java
@@ -0,0 +1,15 @@
+package com.zep.UserAuth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserAuthApplication {
+
+	public static void main(String[] args) {
+		
+		SpringApplication.run(UserAuthApplication.class, args);
+		System.out.println("User Auth");
+	}
+
+}
